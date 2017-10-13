f35afebbbbe61b84956467274a6f64f1032b6930
Fabio Mussap <fabiomussap@yahoo.com>
2017-10-13 15:00:30 +0100

    first commit

--- scripts/update.js
+++ scripts/update.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { spawn } = require('child_process')
+const { spawn } = require('cross-spaw')
 const pkg = require('../package.json')
 
 const dependencies = Object.keys(pkg.dependencies)
