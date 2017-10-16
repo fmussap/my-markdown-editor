15e61750ac162dcc0c634302b2501a36337d644e
Fabio Mussap <fabiomussap@yahoo.com>
2017-10-16 07:34:00 +0100

    update 16-10

--- scripts/update.js
+++ scripts/update.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { spawn } = require('cross-spaw')
+const { spawn } = require('cross-spawn')
 const pkg = require('../package.json')
 
 const dependencies = Object.keys(pkg.dependencies)
